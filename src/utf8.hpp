f88637a01dd8413a534b80f9900a77028b5b977b
Take Vos <info@takevos.nl>
2025-05-23 11:36:49 +0200

    update utf8

--- src/utf8.hpp
+++ src/utf8.hpp
@@ -6,8 +6,62 @@
 #include <cassert>
 #include <utility>
 #include <array>
+#include <string>
 
 namespace hl {
+
+[[nodiscard]] constexpr char32_t decode_utf8_code_point(uint8_t const*& ptr, uint8_t const* ptr_end) noexcept
+{
+    constexpr auto replacement_character = 0xfffd;
+
+    // Should only be called if there are code-units to decode.
+    assert(it != end);
+
+    // Get the first byte.
+    uint64_t cp = *ptr++;
+    if (cp < 128) [[likely]] {
+        return cp;
+    }
+
+    auto count = std::countl_one(static_cast<uint8_t>(cp)) - 1;
+    auto const seq_ptr_end = ptr + count;
+    if (seq_ptr_end > ptr_end) [[unlikely]] {
+        return replacement_character;
+    }
+
+    // Strip the leading bits.
+    cp &= 0x7f >> count;
+
+    uint8_t top_bits = 0x00;
+    #pragma clang loop unroll(disable)
+    for (; ptr != seq_ptr_end; ++ptr) {
+        // Invert the top bit so that we can concatonate the value.
+        auto cu = static_cast<uint8_t>(*ptr) ^ 0x80;
+        cp <<= 6;
+        cp |= cu;
+        top_bits |= cu;
+    }
+
+    if (top_bits != 0) [[unlikely]] {
+        return replacement_character;
+    }
+
+    if (cp > 0x10ffff) [[unlikely]] {
+       // code-point is greater than the maximum valid code-point.
+       return replacement_character;
+    }
+
+    // Using 8 bit per shift value, reduces an (AND r,15) instruction.
+    count *= 8;
+    constexpr auto min_code_point_shifts = uint64_t{0x38'38'38'38'09'04'00'38};
+    auto min_code_point_value = uint64_t{0x80} << ((min_code_point_shifts >> count) & 0xff);
+    if (cp < min_code_point_value) [[unlikely]] {
+        // Invalid sequence count, or overlong encoding.
+        return replacement_character;
+    }
+
+    return static_cast<char32_t>(cp);
+}
 
 /** Decode a single Unicode code-point from a UTF-8 stream.
  *
@@ -25,79 +79,22 @@ template<std::random_access_iterator It>
     requires (std::same_as<typename std::iterator_traits<It>::value_type, char> or
               std::same_as<typename std::iterator_traits<It>::value_type, char8_t>)
 {
-    assert(it != end);
+    auto ptr = reinterpret_cast<uint8_t const *>(&it);
+    auto const end_ptr = reinterpret_cast<uint8_t const *>(&end);
 
-    auto tmp_it = it;
-
-    auto cu = static_cast<uint8_t>(*tmp_it++);
-    if (cu < 128) {
-        it = tmp_it;
-        return cu;
-    }
-
-    auto count = std::countl_one(cu);
-    if (std::distance(it, end) < count) {
-        return U'\uFFFD';
-    }
-    auto const tmp_end = it + count;
-
-    // Strip the leading bits.
-    cu <<= count;
-    cu >>= count;
-
-    auto cp = static_cast<uint64_t>(cu);
-
-    auto mask = uint8_t{};
-    #pragma clang loop unroll(disable)
-    do {
-        assert(tmp_it != tmp_end);
-        cu = static_cast<uint8_t>(*tmp_it++);
-        cu ^= 0x80;
-        mask |= cu;
-        cp <<= 6;
-        cp |= cu;
-    } while (tmp_it != tmp_end);
-
-    constexpr auto replacement_character = 0xfffd;
-
-    mask >>= 6;
-    if (mask != 0x00) {
-        // Top bit of continuation character should all be 1.
-        cp = replacement_character;
-    }
-
-    // The shift values to turn 0x80 into a minimum code-point value:
-    // 0 -> 0
-    // 1 -> 0x80
-    // 2 -> 0x0800
-    // 3 -> 0x01'0000
-    // 4-7 -> 0
-    constexpr auto min_code_point_value_shifts = uint64_t{0x1f'1f'1f'1f'09'04'00'1f};
-
-    assert(count != 7 and count <= 8);
-    auto min_code_point_value_shift = static_cast<uint8_t>(min_code_point_value_shifts >> ((count - 1) * 8));
-
-    auto min_code_point_value = uint32_t{0x80} << min_code_point_value_shift;
-
-    if (cp < min_code_point_value) {
-        // Invalid sequence count, or overlong encoding.
-        cp = replacement_character;
-    }
-    
-    if (cp > 0x10FFFF) {
-        // code-point is greater than the maximum valid code-point.
-        cp = replacement_character;
-    }
-
-    if (cp >= 0xD800 and cp <= 0xDFFF) {
-        // Surrogate code-points are not valid in UTF-8.
-        cp = replacement_character;
-    }
-
-    // Check for invalid code-point.
-    it = tmp_end;
-    return static_cast<char32_t>(cp);
+    auto const ptr_prev = ptr;
+    auto const r = decode_utf8_code_point(ptr, end_ptr);
+    it += std::distance(ptr_prev, ptr);
+    return r;
 }
 
 } // namespace hl
 
+[[nodiscard]] char32_t test(std::string tmp, std::string::const_iterator &out) noexcept
+{
+    auto it = tmp.cbegin();
+    auto const end = tmp.cend();
+    auto const v = hl::decode_utf8_code_point(it, end);
+    out = it;
+    return v;
+}
