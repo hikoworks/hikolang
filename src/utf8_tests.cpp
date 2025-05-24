983032126849924e2eacc584492f17a4aa6c0db2
Take Vos <info@takevos.nl>
2025-05-24 20:21:55 +0200

    improved utf-8

--- src/utf8_tests.cpp
+++ src/utf8_tests.cpp
@@ -7,17 +7,16 @@ TEST_SUITE(utf8_suite)
 
 TEST_CASE(utf8_decode_valid) 
 {
-    auto utf8_str = std::string{"Hello, 世界!"};
+    auto const utf8_str = std::string{"Hello, 世界!"};
     auto utf32_str = std::u32string{};
     auto it = utf8_str.begin();
-    auto end = utf8_str.end();
+    auto const end = utf8_str.end();
 
-    char32_t code_point;
     while (it != end) {
-        code_point = hl::decode_utf8_code_point(it, end);
+        auto const code_point = hl::decode_utf8_code_point(it, end);
         utf32_str.push_back(code_point);
 
-        REQUIRE(code_point != U'\ufffd');
+        REQUIRE(code_point <= 0x10ffff);
     }
 
     REQUIRE(utf32_str == U"Hello, 世界!");
