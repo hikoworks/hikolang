b1e5ea9608b944eddae4e25fc71f72f6a1246a06
Take Vos <info@takevos.nl>
2025-08-12 15:12:34 +0200

    log

--- src/utility/log.cpp
+++ src/utility/log.cpp
@@ -10,10 +10,14 @@ class enable_ansi_codes_helper {
 public:
     ~enable_ansi_codes_helper() = default;
 
-    
+    constexpr enable_ansi_codes_helper() noexcept = default;
 
-    enable_ansi_codes_helper()
+    enable_ansi_codes_helper(bool enable)
     {
+        if (not enable) {
+            _enabled = false;
+        }
+
 #if defined(_WIN32)
         if (not enable_ansi_codes(STD_OUTPUT_HANDLE)) {
             global_log_mask = global_log_mask & ~log_mask::color;
@@ -25,9 +29,17 @@ public:
             return;
         }
 #endif
+        _enabled = true;
+    }
+
+    [[nodiscard]] bool enabled() const noexcept
+    {
+        return _enabled;
     }
 
 private:
+    bool _enabled = false;
+
 #if defined(_WIN32)
     [[nodiscard]] static bool enable_ansi_codes(DWORD device)
     {
@@ -45,12 +57,125 @@ private:
         }
         return true;
     }
-    #endif
+#endif
 };
 
-void enable_ansi_codes()
+[[nodiscard]] bool enable_ansi_codes()
 {
     static auto init = enable_ansi_codes_helper{};
+    return init.enabled();
 }
 
+/** Convert a value to ticks.
+ *
+ * @param value Floating point value between 0.0 and 1.0
+ * @param steps Number of steps between 0.0 and 1.0 (excluding 0.0)
+ * @return The tick number.
+ */
+[[nodiscard]] unsigned int progress::value_to_tick(float value, unsigned int num_ticks)
+{
+    value = std::clamp(value, 0.0f, 1.0f);
+    value *= gsl::narrow<float>(num_ticks);
+
+    // Round the value to the next tick, only when it is 90% on its way
+    // to that tick.
+    value *= 10.0f;
+    value = std::round(value);
+    value /= 10.0f;
+    value = std::floor(value);
+
+    return gsl::narrow<unsigned int>(value);
 }
+
+/** Update the display on a non-ANSI capable terminal
+ *
+ * It shows the message, followed by percentages, increasing by 20%.
+ * For example:
+ *    `My progress message. 20% 40% 60%`
+ *
+ * This version can not handle:
+ *  - When the message changes, only the first message is displayed.
+ */
+void progress::update_display_linear() const
+{
+    auto flush = false;
+
+    if (_old_msg.empty()) {
+        std::print(stderr, "{}", _msg);
+        flush = true;
+    }
+
+    // Ticks every 10%.
+    auto const ticks = value_to_tick(_value, 5);
+    auto const old_ticks = value_to_tick(_value, 5);
+    if (old_ticks < ticks) {
+        std::print(stderr, " {}%", ticks * 20);
+        flush = true;
+    }
+
+    if (flush) {
+        std::fflush(stderr);
+    }
+}
+
+/** Update the display on a ANSI capable terminal.
+ *
+ * Display looks as follows:
+ *  `[====      ] My progress message.`
+ */
+void progress::update_display_color() const
+{
+    auto flush = false;
+    auto first_time = _old_msg.empty();
+
+    auto const ticks = value_to_tick(_value, 10);
+    auto const old_ticks = value_to_tick(_value, 10);
+    if (old_ticks < ticks or first_time) {
+        assert(ticks <= 10);
+
+        auto bar = std::string{};
+        auto i = 0u;
+        for (; i != ticks; ++i) {
+            bar += '=';
+        }
+        for (; i != 10; ++i) {
+            bar += " ";
+        }
+
+        // ANSI: go to column 1.
+        std::print(stderr, "\033[G [{}]", bar);
+        flush = true;
+    }
+
+    if (_old_msg != _msg) {
+        // ANSI: go to column 15. clear to end of line.
+        std::print(stderr, "\033[15G\033[K{}", _msg);
+        flush = true;
+    }
+
+    if (flush) {
+        std::fflush(stderr);
+    }
+}
+
+void progress::update_display() const
+{
+    if (to_bool(global_log_mask & log_mask::progress) and to_bool(global_log_mask & log_mask::color)) {
+        if (not enable_ansi_codes()) {
+            global_log_mask &= ~log_mask::color;
+        }
+    }
+
+    if (to_bool(global_log_mask & log_mask::progress)) {
+        if (to_bool(global_log_mask & log_mask::color)) {
+            update_display_color();
+        } else {
+            update_display_linear();
+        }
+    }
+
+    _old_msg = _msg;
+    _old_value = _value;
+}
+
+} // namespace hk
--- src/utility/log.hpp
+++ src/utility/log.hpp
@@ -11,11 +11,11 @@
 namespace hk {
 
 /** Enable ANSI codes on the terminal.
- * 
+ *
  * @note If unable to configure the terminal for ANSI, the log_mask::color
  *       flag is removed.
  */
-void enable_ansi_codes();
+[[nodiscard]] bool enable_ansi_codes();
 
 enum class log_mask {
     /** Display progress for downloading.
@@ -54,6 +54,18 @@ enum class log_mask {
 [[nodiscard]] constexpr log_mask operator~(log_mask rhs) noexcept
 {
     return static_cast<log_mask>(~std::to_underlying(rhs));
+}
+
+constexpr log_mask &operator|=(log_mask& lhs, log_mask rhs) noexcept
+{
+    lhs = lhs | rhs;
+    return lhs;
+}
+
+constexpr log_mask &operator&=(log_mask& lhs, log_mask rhs) noexcept
+{
+    lhs = lhs & rhs;
+    return lhs;
 }
 
 [[nodiscard]] constexpr bool to_bool(log_mask rhs) noexcept
@@ -158,112 +170,34 @@ private:
     mutable float _old_value = 0.0f;
 
     /** Convert a value to ticks.
-     * 
+     *
      * @param value Floating point value between 0.0 and 1.0
      * @param steps Number of steps between 0.0 and 1.0 (excluding 0.0)
      * @return The tick number.
      */
-    [[nodiscard]] static unsigned int value_to_tick(float value, unsigned int num_ticks)
-    {
-        value = std::clamp(value, 0.0f, 1.0f);
-        value *= gsl::narrow<float>(num_ticks);
-
-        // Round the value to the next tick, only when it is 90% on its way
-        // to that tick. 
-        value *= 10.0f;
-        value = std::round(value);
-        value /= 10.0f;
-        value = std::floor(value);
-
-        return gsl::narrow<unsigned int>(value);
-    }
+    [[nodiscard]] static unsigned int value_to_tick(float value, unsigned int num_ticks);
 
     /** Update the display on a non-ANSI capable terminal
-     * 
+     *
      * It shows the message, followed by percentages, increasing by 20%.
      * For example:
      *    `My progress message. 20% 40% 60%`
-     * 
+     *
      * This version can not handle:
      *  - When the message changes, only the first message is displayed.
      */
-    void update_display_linear() const
-    {
-        auto flush = false;
-
-        if (_old_msg.empty()) {
-            std::print(stderr, "{}", _msg);
-            flush = true;
-        }
-
-        // Ticks every 10%.
-        auto const ticks = value_to_tick(_value, 5);
-        auto const old_ticks = value_to_tick(_value, 5);
-        if (old_ticks < ticks) {
-            std::print(stderr, " {}%", ticks * 20);
-            flush = true;
-        }
-
-        if (flush) {
-            std::fflush(stderr);
-        }
-    }
+    void update_display_linear() const;
 
     /** Update the display on a ANSI capable terminal.
-     * 
+     *
      * Display looks as follows:
      *  `[====      ] My progress message.`
+     *  `[====OK====] My progress message.`
+     *  `[   FAIL   ] My progress message.`
      */
-    void update_display_color() const
-    {
-        enable_ansi_codes();
+    void update_display_color() const;
 
-        auto flush = false;
-        auto first_time = _old_msg.empty();
-
-        auto const ticks = value_to_tick(_value, 10);
-        auto const old_ticks = value_to_tick(_value, 10);
-        if (old_ticks < ticks or first_time) {
-            assert(ticks <= 10);
-
-            auto bar = std::string{};
-            auto i = 0u;
-            for (; i != ticks; ++i) {
-                bar += '=';
-            }
-            for (; i != 10; ++i) {
-                bar += " ";
-            }
-            
-            // ANSI: go to column 1.
-            std::print(stderr, "\033[G [{}]", bar);
-            flush = true;
-        }
-
-        if (_old_msg != _msg) {
-            // ANSI: go to column 15. clear to end of line.
-            std::print(stderr, "\033[15G\033[K{}", _msg);
-            flush = true;
-        }
-
-        if (flush) {
-            std::fflush(stderr);
-        }
-    }
-
-    void update_display() const
-    {
-        if (to_bool(global_log_mask & log_mask::progress)) {
-            if (to_bool(global_log_mask & log_mask::color)) {
-                update_display_color();
-            } else {
-                update_display_linear();
-            }
-        }
-
-        _old_msg = _msg;
-        _old_value = _value;
-    }
+    void update_display() const;
 };
 
 } // namespace hk
