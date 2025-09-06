684f76b4d036d30cb2003e9b4f13ad7270c893af
Take Vos <info@takevos.nl>
2025-09-06 13:58:55 +0200

    fix base32

--- src/utility/base32.cpp
+++ src/utility/base32.cpp
@@ -18,23 +18,27 @@ namespace hk {
 
 [[nodiscard]] std::string base32_encode(std::span<char const> value)
 {
-    auto const r_size = (value.size() * 8 + 4) / 5;
     auto r = std::string{};
-    r.resize(r_size);
+    r.resize((value.size() * 8 + 4) / 5);
 
     auto j = 0uz;
-    for (auto i = 0uz; i != r_size; ++i, j += 5) {
+    auto i = 0uz;
+    while (i != r.size()) {
         auto const byte_nr = j / 8;
         auto const bit_nr = j % 8;
+        j += 5;
 
-        auto bits = uint16_t{0};
-        bits |= value[byte_nr + 1];
-        bits <<= 8;
+        auto bits = uint16_t{};
+        if (byte_nr + 1 < value.size()) {
+            bits |= value[byte_nr + 1];
+            bits <<= 8;
+        }
+        assert(byte_nr < value.size());
         bits |= value[byte_nr];
         bits >>= bit_nr;
         bits &= 0x1f;
 
-        r[i] = base32_encode_5bit(static_cast<unsigned char>(bits));
+        r[i++] = base32_encode_5bit(static_cast<unsigned char>(bits));
     }
 
     return r;
