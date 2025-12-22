06f2d6903d9614d1a2d31ba9a3ea0f987dc68962
Take Vos <info@takevos.nl>
2025-12-22 14:08:25 +0100

    module_list

--- src/repository/module_list.cpp
+++ src/repository/module_list.cpp
@@ -5,7 +5,9 @@ namespace hk {
 
 void module_list::add(source& s)
 {
-
+    auto it = std::lower_bound(_sources.begin(), _sources.end(), s, [](auto const& item, auto const& x) {
+        return cmp_names(*item, *x) <=> std::strong_ordering::less;
+    });
 }
 
 }
